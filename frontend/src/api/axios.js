8f7b2d8f63e18a0eec539233b730aca8e5b8dfc2
Nasheer <nasheer24555a4406@gmail.com>
2026-07-05 14:10:06 +0530

    Point frontend to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://skincare-backend-abc1.onrender.com/api",
 });
 
 // Attach JWT token to every request if present
