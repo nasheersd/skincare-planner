9dad15841667261b3b470a5ec899ce03c2259439
Nasheer <nasheer24555a4406@gmail.com>
2026-07-24 15:17:38 +0530

    update password length frontend rule to match 6-character schema

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -125,8 +125,8 @@ export default function Register() {
             <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div className="field">
-            <label htmlFor="password">Password <span className="hint">min 12 characters</span></label>
-            <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required minLength={12} maxLength={72} />
+            <label htmlFor="password">Password <span className="hint">min 6 characters</span></label>
+            <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required minLength={6} maxLength={72} />
           </div>
           <div className="field">
             <label htmlFor="role">I am a</label>
